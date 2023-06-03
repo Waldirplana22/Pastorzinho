82fe79686d137ec98c4039e2a9b93e27fe40b15c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-02 22:11:29 -0300

    Pastorzinho

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-# Pastorzinho
- Console.WriteLine ("Pastorzinho \n");
+﻿ Console.WriteLine ("Pastorzinho \n");
    string A = "Dó, ré, mi, fá, fá, fá,";    
     string B = "Dó, ré, dó, ré, ré, ré,";
     string C = "Dó, sol, fá, mi, mi, mi,";
